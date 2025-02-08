145226170d1e95d5dd0ed40ad27ec585064b3cdc
overwhelme <3080972168@qq.com>
2025-02-08 13:50:12 +0800

    refs:推送项目初步内容

---
+++ System/Delay.c
@@ -0,0 +1,26 @@
+#include "Delay.h"
+
+void Delay_us(uint32_t us)
+{
+	SysTick->LOAD = 72 * us;  //设置定时器重装值
+	SysTick->VAL = 0x00;			//清空当前定时器
+	SysTick->CTRL = 0x00000005;	//设置时钟源为HCLK，启动定时器
+	while(!(SysTick->CTRL & 0x00010000)); //等待技术到0
+	SysTick->CTRL = 0x00000004; //关闭定时器
+}
+
+void Delay_ms(uint32_t ms)
+{
+	while(ms--)
+	{
+		Delay_us(1000);
+	}
+}
+
+void Delay_s(uint32_t s)
+{
+	while(s--)
+	{
+		Delay_ms(1000);
+	}
+}
